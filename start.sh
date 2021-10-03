9d91bcae28033a0ce1fce0d16d8dcf886011cdbf
subinps <64341611+subinps@users.noreply.github.com>
2021-10-03 22:14:22 +0530

    fix

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 echo "Cloning Repo...."
-if [[ $BRANCH != "None" ]]
+if [ -z $BRANCH ]
 then
-  echo "Cloning beta branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
-else
   echo "Cloning main branch...."
   git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+else
+  echo "Cloning $BRANCH branch...."
+  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
