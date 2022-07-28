e91a3b04f569fce52ae900fc7779fba89a295060
xD-slayer <95204708+xD-slayer@users.noreply.github.com>
2022-07-28 15:34:31 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,8 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://T.me//XDSLAYER'),
+        InlineKeyboardButton('🧩 Join Here', url='https://t.me//XDSLAYER'),
     ]
     ]
 def get_cmd(dur):
